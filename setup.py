194f7ab063e6fdee2ce1ab8f64869356f150da6b
Wesenheit <matio@opoczta.pl>
2022-11-05 00:54:21 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     package_dir={"":"src"},
     package_data={"Iris.filters":["*.dat"]},
     install_requiers=['numpy,scipy,matplotlib,seaborn,emcee'],
-    description="Python library for solving tasks from Project Euler",
+    description="Python library for assembling SED",
     #include_package_data=True,
     license="MIT"
 )
